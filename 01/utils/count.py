89198e3e9b6bff8dc37306f4f768c8882098871d
ke777777 <keeee7738@icloud.com>
2023-11-08 13:40:20 +0900

    bank_account作成

--- 01/utils/count.py
+++ 01/utils/count.py
@@ -2,9 +2,9 @@ from .function import add
 
 def count_word(input_str,x):
     assert isinstance(input_str,str)
-    assert isinstance(x,str),len(x) == 1
+    assert isinstance(x,str) and len(x) == 1
     count = 0
-    for char in input_str:
-        if char == x:
-            count += 1
+    for i in range(len(input_str)):
+        if input_str[i] == x:
+            count = add(count,1)
     return count
