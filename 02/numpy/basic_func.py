ecaa28a5b65d703907af8a924e0b867957660ca6
ke777777 <keeee7738@icloud.com>
2023-11-08 16:45:09 +0900

    numpy python完成ver2

---
+++ 02/numpy/basic_func.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+a = np.asarray([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+
+
+print(np.mean(a))
+print(a.mean())
+print(np.mean(a,axis=0))
+print(np.mean(a,axis=1))
+print(np.sum(a))
+print(np.sum(a,axis=0))
+print(np.sum(a,axis=1))
+print(np.max(a))
+print(np.min(a))
